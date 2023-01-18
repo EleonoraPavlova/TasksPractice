f5a8c9c3a5362dbd85a2b04bd84894a5df62fdbb
eleonora <nebrudko18@gmail.com>
2023-01-18 20:54:25 +0100

    added finished work

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<div class="container">
+		<OneCard
+			:image="option"
+			v-for="(option, index) in options"
+			:key="option"
+			@click="expend(index)"
+			:class="{ active: activeItemIndex === index }"
+			:isActive="activeItemIndex === index"
+		/>
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import OneCard from "./components/OneCard.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+	name: "App",
+	components: {
+		OneCard,
+	},
+	data() {
+		return {
+			options: [],
+			activeItemIndex: null,
+		};
+	},
+	async created() {
+		try {
+			const response = await fetch("/data.json");
+			const options = await response.json();
+			this.options = options.car;
+		} catch (err) {
+			console.log(err.name);
+		}
+	},
+	methods: {
+		expend(ind) {
+			this.activeItemIndex = ind;
+		},
+	},
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+ <style lang="scss">
+.container {
+	width: 100%;
+	display: flex;
+	padding: 0 25px;
 }
 </style>
---
+++ src/components/OneCard.vue
@@ -0,0 +1,57 @@
+<template>
+	<div
+		class="slide"
+		:style="{ backgroundImage: `url(${image.link})` }"
+		@click="$emit('click')"
+	>
+		<h3 class="slide__h3" :class="{ active: isActive }">{{ image.name }}</h3>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "OneCard",
+	props: {
+		image: {
+			type: Object,
+			default: null,
+		},
+		isActive: {
+			type: Boolean,
+		},
+	},
+
+	emits: ["click"],
+};
+</script>
+
+<style lang="scss">
+.slide {
+	height: 80vh;
+	border-radius: 20px;
+	margin: 10px;
+	cursor: pointer;
+	color: white;
+	padding: 8px;
+	flex: 1;
+	background-position: center;
+	background-size: cover;
+	background-repeat: no-repeat;
+	transition: all 500ms ease-in-out;
+	display: flex;
+	align-items: end;
+	&__h3 {
+		padding: 10px;
+		font-size: 24px;
+		margin: 5px;
+		opacity: 0;
+		&.active {
+			opacity: 1;
+			transition: opacity 0.3s ease-in 0.4s;
+		}
+	}
+	&.active {
+		flex: 20;
+	}
+}
+</style>
