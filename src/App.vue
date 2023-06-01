e92522820c765a774bbf9cadf77d0f7a09fad17c
eleonora <nebrudko18@gmail.com>
2023-06-01 13:32:39 +0300

    little fix

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,51 @@
 <template>
-	<div class="container">
-		<OneCard
-			:image="option"
-			v-for="(option, index) in options"
-			:key="option"
-			@click="expend(index)"
-			:class="{ active: activeItemIndex === index }"
-			:isActive="activeItemIndex === index"
-		/>
-	</div>
+  <div class="container">
+    <OneCard
+      :image="option"
+      v-for="(option, index) in options"
+      :key="option"
+      @click="expend(index)"
+      :class="{ active: activeItemIndex === index }"
+      :isActive="activeItemIndex === index"
+    />
+  </div>
 </template>
 
 <script>
 import OneCard from "./components/OneCard.vue";
 
 export default {
-	name: "App",
-	components: {
-		OneCard,
-	},
-	data() {
-		return {
-			options: [],
-			activeItemIndex: null,
-		};
-	},
-	async created() {
-		try {
-			const response = await fetch("/public/data.json");
-			const options = await response.json();
-			this.options = options.car;
-		} catch (err) {
-			console.log(err.name);
-		}
-	},
-	methods: {
-		expend(ind) {
-			this.activeItemIndex = ind;
-		},
-	},
+  name: "App",
+  components: {
+    OneCard,
+  },
+  data() {
+    return {
+      options: [],
+      activeItemIndex: null,
+    };
+  },
+  async created() {
+    try {
+      const response = await fetch("/public/data.json");
+      const options = await response.json();
+      this.options = options.car;
+    } catch (err) {
+      console.log(err.name);
+    }
+  },
+  methods: {
+    expend(ind) {
+      this.activeItemIndex = ind;
+    },
+  },
 };
 </script>
 
  <style lang="scss">
 .container {
-	width: 100%;
-	display: flex;
-	padding: 0 25px;
+  width: 100%;
+  display: flex;
+  padding: 0 25px;
 }
 </style>
